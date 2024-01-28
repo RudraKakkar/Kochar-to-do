93c05997c87b18d65452c0d5ef3f944b3f00ce23
RudraKakkar <merudrakakkar@gmail.com>
2024-01-28 22:38:07 +0530

    till props

---
+++ src/components/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+
+    <center>
+          
+      <h1 className='mt-3'>TO-DO LIST</h1>
+        </center>
+
+  )
+}
+
+export default Todo
